7b357e73b2b6adf7480d838448ed7db529d2d04c
WSL_SCD <49774633+Acid-Free@users.noreply.github.com>
2023-03-06 17:51:22 +0800

    Add data fetching and insertion tests

--- test/jobPosting.test.js
+++ test/jobPosting.test.js
@@ -1,4 +1,4 @@
-import * as chai from "chai";
+import chai, { assert } from "chai";
 import chaiHttp from "chai-http";
 import JobPosting from "../server/models/jobPosting.js";
 import server from "../server/app.js";
@@ -7,6 +7,67 @@ process.env.NODE_ENV = "test";
 
 chai.use(chaiHttp);
 
-describe("/Data fetching and insertion", () => {});
+// empties the selected DB after executing all tests
+after((done) => {
+  JobPosting.deleteMany({}, (error) => {});
+  done();
+});
+
+describe("/Data fetching and insertion", () => {
+  it("should have 0 job posting in the beginning", (done) => {
+    chai
+      .request(server)
+      .get("/api/job-postings/")
+      .end((error, response) => {
+        assert.equal(response.status, 200);
+        assert.typeOf(response.body, "array");
+        assert.lengthOf(response.body, 0);
+        done();
+      });
+  });
+
+  let newObjectId;
+
+  it("should have 1 job posting after posting", (done) => {
+    const jobPosting = {
+      title: "Rocksta programma 5",
+      description: "Looking for best programma",
+      location: "World",
+      salary: "999999",
+      company: {
+        name: "Best Company Name",
+        website: "https://best-company-name.com",
+      },
+    };
+
+    chai
+      .request(server)
+      .post("/api/job-postings/")
+      .send(jobPosting)
+      .end((error, response) => {
+        assert.equal(response.status, 201);
+
+        newObjectId = response.body._id;
+
+        done();
+      });
+  });
+
+  it("should update an existing job posting", (done) => {
+    const newJobPosting = {
+      title: "Rockstar Programmer",
+      description: "Looking for the best programmer",
+    };
+    chai
+      .request(server)
+      .put(`/api/job-postings/${newObjectId}`)
+      .send(newJobPosting)
+      .end((error, response) => {
+        assert.equal(response.status, 200);
+
+        done();
+      });
+  });
+});
 
 describe("/Data deletion", () => {});
